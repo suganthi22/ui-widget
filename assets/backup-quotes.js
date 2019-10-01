7d67fd41460bc87581694a9956d04baeac11d09f
suganthi22 <50652406+suganthi22@users.noreply.github.com>
2019-10-01 17:55:42 +0530

    Create backup-quotes.js

---
+++ assets/backup-quotes.js
@@ -0,0 +1,39 @@
+[
+    {
+      "content": "The Creator has not given you a longing to do that which you have no ability to do.",
+      "author": "Orison Marden"
+    },
+    {
+      "content": "The fox has many tricks. The hedgehog has but one. But that is the best of all.",
+      "author": "Desiderius Erasmus"
+    },
+    {
+      "content": "Self-complacency is fatal to progress.",
+      "author": "Margaret Sangster"
+    },
+    {
+      "content": "To ensure good health: eat lightly, breathe deeply, live moderately, cultivate cheerfulness, and maintain an interest in life.",
+      "author": "William Londen"
+    },
+    {
+      "content": "The amount of happiness that you have depends on the amount of freedom you have in your heart.",
+      "author": "Thich Nhat Hanh"
+    },
+    {
+      "content": "We do not quit playing because we grow old, we grow old because we quit playing.",
+      "author": "Oliver Holmes"
+    },
+    {
+      "content": "What the caterpillar calls the end of the world, the master calls a butterfly.",
+      "author": "Richard Bach"
+    },
+    {
+      "content": "There is one thing you have got to learn about our movement. Three people are better than no people.",
+      "author": "Fannie Hamer"
+    },
+    {
+        "content": "A computer lets you make more mistakes faster than any other invention in human history, with the possible exceptions of handguns and tequila.",
+        "author": "Mitch Ratcliffe"
+    }
+  ]
+  
